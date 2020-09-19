784fb3a91f3b01ece9deeb68cc383d074c9fd1b0
Lintang Prayogo <linrangprayogo12@gmail.com>
2020-09-19 14:19:36 +0700

    NavGraph

---
+++ app/src/main/java/com/lintangprayogo/searchapp/ui/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.lintangprayogo.searchapp.ui
+
+import androidx.fragment.app.Fragment
+import com.lintangprayogo.searchapp.R
+
+class GalleryFragment:Fragment(R.layout.fragment_gallery) {
+
+}
