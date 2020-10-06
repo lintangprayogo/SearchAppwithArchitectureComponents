991cf25653401854a45315a09090b940f59b7917
Lintang Prayogo <linrangprayogo12@gmail.com>
2020-10-07 00:00:06 +0700

    Loading State

--- app/src/main/java/com/lintangprayogo/searchapp/ui/GalleryFragment.kt
+++ app/src/main/java/com/lintangprayogo/searchapp/ui/GalleryFragment.kt
@@ -6,9 +6,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.observe
+import androidx.paging.LoadState
 import com.lintangprayogo.searchapp.R
 import com.lintangprayogo.searchapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,56 +19,80 @@ import dagger.hilt.android.AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val viewModel by viewModels<GalleryViewModel>()
 
-    private var _binding:FragmentGalleryBinding? =null
-    private  val binding get()=_binding!!
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentGalleryBinding.bind(view)
 
-        val adapter =UnsplashAdapter()
+        val adapter = UnsplashAdapter()
 
         binding.apply {
             rvSplash.setHasFixedSize(true)
-            rvSplash.adapter=adapter.withLoadStateHeaderAndFooter(
-                header= UnsplashResultLoadStateAdapter{adapter.retry()},
-                footer = UnsplashResultLoadStateAdapter{adapter.retry()}
+            rvSplash.itemAnimator = null
+            rvSplash.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnsplashResultLoadStateAdapter { adapter.retry() },
+                footer = UnsplashResultLoadStateAdapter { adapter.retry() },
             )
+            btnRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.results().observe(viewLifecycleOwner) {
-              adapter.submitData(viewLifecycleOwner.lifecycle,it)
+            Log.d("TAG", "onViewCreated: ")
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        adapter.addLoadStateListener { loadstate ->
+                binding.apply {
+                    progressBar.isVisible = loadstate.source.refresh is LoadState.Loading
+                    rvSplash.isVisible = loadstate.source.refresh is LoadState.NotLoading
+                    btnRetry.isVisible = loadstate.source.refresh is LoadState.Error
+                    tvError.isVisible = loadstate.source.refresh is LoadState.Error
+
+
+                    if (loadstate.source.refresh is LoadState.NotLoading && loadstate.append.endOfPaginationReached && adapter.itemCount<1){
+                        rvSplash.isVisible = false
+                        tvEmpty.isVisible  =true
+                    }else {
+                        tvEmpty.isVisible  =false
+                    }
+
+                }
         }
         setHasOptionsMenu(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_gallery,menu)
 
-        val searchItem  = menu.findItem(R.id.search)
-        val searchView  = searchItem.actionView as SearchView
+        inflater.inflate(R.menu.menu_gallery, menu)
 
-        searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+        val searchItem = menu.findItem(R.id.search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if(query !=null){
+
+                if (query != null) {
                     binding.rvSplash.scrollToPosition(0)
                     viewModel.searchResults(query)
-                    binding.rvSplash.clearFocus()
+                    searchView.clearFocus()
                 }
-                return  true
+                return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                return  true
+                return true
             }
-
         })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding=null
+        _binding = null
     }
 }
